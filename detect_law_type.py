461978544a28327b2a7206188d81aa3d3edb5025
UUU, BluePain <adamaakif23@gmail.com>
2026-01-19 01:32:02 +0100

    Add methods for legal functions and preparation

--- detect_law_type.py
+++ detect_law_type.py
@@ -4,7 +4,27 @@ import pandas as pd
 class Law:
   def __init__(self, law: list[str] | None):
     self.law = law
-  
+    self.ready = False
+    
+  def __prepare():
+    RuleClf = joblib.load('Rule.Clf')
+    rule_soft_type = RuleClf.predict_probs(self.law)
+    rule_soft_type = pd.DataFrame(rule_soft_type, columns=RuleClf.classes_)
+    rule_soft_type.insert(0, 'sents', self.law)
+    self.rule_soft_type = rule_soft_type
+    rule_hard_type = RuleClf.predict(self.law)
+    rule_hard_type = pd.DataFrame({'sents': self.law, 'label': rule_hard_type})
+    self.rule_hard_type = rule_hard_type
+    self.defs = self.rule_hard_type[rules_hard_type['label'] == 'Definition']['sents']
+    self.oblgs = self.rule_hard_type[rules_hard_type['label'] == 'Obligation']['sents']
+    self.prohs = self.rule_hard_type[rules_hard_type['label'] == 'Prohibition']['sents']
+    self.sancs = self.rule_hard_type[rules_hard_type['label'] == 'Sanction']['sents']
+    self.exceps = self.rule_hard_type[rules_hard_type['label'] == 'Exception']['sents']
+    
+    # prepared
+    
+    self.ready = True
+
   def rules_types(self) -> pd.DataFrame:
     if not self.law:
       raise Exception("Law List Is Empty")
@@ -37,5 +57,49 @@ class Law:
     else:
       return df[['Law Type', 'Percentage']].sort_values(by=['Percentage'], ascending=False).reset_index(drop=True)
 
+  def legal_function():
+    if self.ready:
+      return self.rule_hard_type
+    self.__prepare()
+    return self.rule_hard_type
+
+  def legal_function_probs():
+    if self.ready:
+      return self.rule_soft_type
+    self.__prepare()
+    return self.rule_soft_type
+
+  def definitions():
+    if self.ready:
+      return self.defs
+    self.__prepare()
+    return self.defs
+
+  def obligations():
+    if self.ready:
+      return self.oblgs
+    self.__prepare()
+    return self.oblgs
+
+  def prohibitions():
+    if self.ready:
+      return self.prohs
+    self.__prepare()
+    return self.prohs
+    
+  def sanctions():
+    if self.ready:
+      return self.sancs
+    self.__prepare()
+    return self.sancs
+    
+  def exceptions():
+    if self.ready:
+      return self.exceps
+    self.__prepare()
+    return self.exceps
+    
+    
+    
 
 
